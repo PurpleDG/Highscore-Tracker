c65da13dc7b3ef0979cbb7f096e7811378870908
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-02-25 13:08:20 -0700

    Finished main function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Evan McCabe, Asher Wangia, Connor Pavicic, Gavin Pierce Highscore Tracker
+def main():
+	while True:
+		choice = input("""Which one do you want to do?:
+1: Play a game
+2: Display high scores
+3: Exit the program
+	
+: """)
+
+		if choice == '1':
+			# Run Gavin's functions
+			pass
+		elif choice == '2':
+			# Run Asher's function
+			pass
+		elif choice == '3':
+			print('Thanks for playing!')
+			break
