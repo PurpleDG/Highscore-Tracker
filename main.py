ab28091cff656a6c7aa4b34242818165dc904dcc
PurpleDG <evan.mccabe@ucas-edu.net>
2025-02-25 13:44:19 -0700

    Just starting...

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Evan McCabe, Asher Wangia, Connor Pavicic, Gavin Pierce Highscore Tracker
+# Evan McCabe Highscore Tracker Set High Score Function
+
+#Tic-Tac-Toe and Number Guessing Game
+
+highScore1 = {
+    "name": "None",
+    "score": 0
+},
+highScore2 = {
+    "name": "None",
+    "score": 0
+},
+highScore3 = {
+    "name": "None",
+    "score": 0
+}
+
+ticTacToe = [highScore1, highScore2, highScore3]
