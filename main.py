c678cbf59e1895a827dfbf124c4c32b8bd6ccd6b
PurpleDG <evan.mccabe@ucas-edu.net>
2025-02-26 13:17:35 -0700

    I finished the "Set High Score" function for our high score tracker program.

--- main.py
+++ main.py
@@ -2,17 +2,117 @@
 
 #Tic-Tac-Toe and Number Guessing Game
 
-highScore1 = {
+#Tic-Tac-Toe high score variables:
+TTThighScore1 = {
     "name": "None",
     "score": 0
-},
-highScore2 = {
+}
+TTThighScore2 = {
     "name": "None",
     "score": 0
-},
-highScore3 = {
+}
+TTThighScore3 = {
     "name": "None",
     "score": 0
 }
 
-ticTacToe = [highScore1, highScore2, highScore3]
+#Number Guessing Game high score variables:
+NGGhighScore1 = {
+    "name": "None",
+    "score": 0
+}
+NGGhighScore2 = {
+    "name": "None",
+    "score": 0
+}
+NGGhighScore3 = {
+    "name": "None",
+    "score": 0
+}
+
+#Set High Score Function:
+def setHighScore(game, name, score):
+
+	#If the function is being called for Tic-Tac-Toe:
+    if game == "Tic-Tac-Toe":
+        
+        #if current score is greater than high score 1:
+        if score >= TTThighScore1["score"]:
+            
+            #Transfer high score 2’s value to high score 3:
+            TTThighScore3["name"] = TTThighScore2["name"]
+            TTThighScore3["score"] = TTThighScore2["score"]
+
+            #Transfer high score 1’s value to high score 2:
+            TTThighScore2["name"] = TTThighScore1["name"]
+            TTThighScore2["score"] = TTThighScore1["score"]
+
+            #Transfer the value of the current score to high score 1:
+            TTThighScore1["name"] = name
+            TTThighScore1["score"] = score
+
+        #if current score is not greater than high score 1:
+        else:
+
+            #if current score is greater than high score 2:
+            if score >= TTThighScore2["score"]:
+
+                #Transfer high score 2’s value to high score 3:
+                TTThighScore3["name"] = TTThighScore2["name"]
+                TTThighScore3["score"] = TTThighScore2["score"]
+
+                #Transfer the value of the current score to high score 2:
+                TTThighScore2["name"] = name
+                TTThighScore2["score"] = score
+
+            #if current score is not greater than high score 2:
+            else:
+
+                #if current score is greater than high score 3:
+                if score >= TTThighScore3["score"]:
+
+                    #Transfer the value of the current score to high score 3:
+                    TTThighScore3["name"] = name
+                    TTThighScore3["score"] = score
+
+    #If the function is being called for Number Guessing Game:
+    elif game == "Number Guessing Game":
+        
+        #if current score is greater than high score 1:
+        if score >= NGGhighScore1["score"]:
+            
+            #Transfer high score 2’s value to high score 3:
+            NGGhighScore3["name"] = NGGhighScore2["name"]
+            NGGhighScore3["score"] = NGGhighScore2["score"]
+
+            #Transfer high score 1’s value to high score 2:
+            NGGhighScore2["name"] = NGGhighScore1["name"]
+            NGGhighScore2["score"] = NGGhighScore1["score"]
+
+            #Transfer the value of the current score to high score 1:
+            NGGhighScore1["name"] = name
+            NGGhighScore1["score"] = score
+
+        #if current score is not greater than high score 1:
+        else:
+
+            #if current score is greater than high score 2:
+            if score >= NGGhighScore2["score"]:
+
+                #Transfer high score 2’s value to high score 3:
+                NGGhighScore3["name"] = NGGhighScore2["name"]
+                NGGhighScore3["score"] = NGGhighScore2["score"]
+
+                #Transfer the value of the current score to high score 2:
+                NGGhighScore2["name"] = name
+                NGGhighScore2["score"] = score
+
+            #if current score is not greater than high score 2:
+            else:
+
+                #if current score is greater than high score 3:
+                if score >= NGGhighScore3["score"]:
+
+                    #Transfer the value of the current score to high score 3:
+                    NGGhighScore3["name"] = name
+                    NGGhighScore3["score"] = score
