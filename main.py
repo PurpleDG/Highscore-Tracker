f660b086728607610e570b8ddc4361891bf8160b
PurpleDG <evan.mccabe@ucas-edu.net>
2025-02-26 13:45:08 -0700

    Added Comments

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-#Connor Pavicic, main_function
+#Connor Pavicic, main function
 
-def main(): #Main function
+#Main function to run the program:
+def main():
+	
+    #Keep the program running in a loop:
 	while True:
+		
+        #Ask the user if they want to play a game, display high scores, or exit the program:
 		choice = input("""Which one do you want to do?:
 1: Play a game
 2: Display high scores
 3: Exit the program
-	
-: """)
+""")
 
+        #If the user chose to play a game:
 		if choice == '1':
 			# Run Gavin's functions
 			pass
+		
+        #If the user chose to display high scores:
 		elif choice == '2':
 			# Run Asher's function
 			pass
+		
+        #If the user chose to exit the program:
 		elif choice == '3':
-			print('Thanks for playing!')
-			break
+			print('Thanks for playing!\n')
+			exit()
